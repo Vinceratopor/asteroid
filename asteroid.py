d231dde15822275afae28a6f66137681e3baebca
vincent Cardinal <vincec.1207@gmail.com>
2023-07-07 14:15:32 +0200

    fin class asteroid

--- asteroid.py
+++ asteroid.py
@@ -9,24 +9,33 @@ Created on Fri Jul  7 13:29:51 2023
 import random
 
 class Asteroid:
-  def __init__(self,speed,angle,size):
-    self.speed = speed
-    self.angle = angle
-    self.size = size
+    def __init__(self, x, y, size):
+        self.x = x  # Coordonnée x
+        self.y = y  # Coordonnée y
+        self.size = size  # Taille de l'astéroïde
 
-  def getSpeed(self):
-    return self.speed
-  def getAngle(self):
-    return self.angle
-  def getSize(self):
-    return self.size
+        self.speed_x = random.uniform(-2, 2)  # Vitesse de déplacement en x (aléatoire)
+        self.speed_y = random.uniform(-2, 2)  # Vitesse de déplacement en y (aléatoire)
 
-  def setSpeed(self,speed):
-    self.speed = speed
-  def setAngle(self,angle):
-    self.angle = angle
-  def setSpeed(self,size):
-    self.size = size
+    def update(self, delta_time):
+        # Mettre à jour la position en fonction de la vitesse et du temps écoulé
+        self.x += self.speed_x * delta_time
+        self.y += self.speed_y * delta_time
 
-  def gettingShot(self):
-    Asteroid a1(self.speed,random.randint(1, 360), self.size-1)
+    def hit(self):
+        # Méthode appelée lorsque l'astéroïde est touché
+        # Implémentez ici le comportement souhaité lorsque l'astéroïde est touché par un tir
+        # Par exemple, vous pouvez le diviser en plusieurs astéroïdes plus petits
+
+        # Exemple de comportement : Diviser l'astéroïde en deux astéroïdes plus petits
+        if self.size > 1:
+            new_size = self.size - 1
+            new_asteroid1 = Asteroid(self.x, self.y, new_size)
+            new_asteroid2 = Asteroid(self.x, self.y, new_size)
+            return [new_asteroid1, new_asteroid2]
+        else:
+            # Si l'astéroïde est de taille 1 ou plus petite, il est détruit
+            return []
+      
+    
+    
--- main.py
+++ main.py
@@ -5,10 +5,29 @@ Created on Fri Jul  7 13:29:15 2023
 
 @author: stagiaire_letg
 """
+from asteroid import Asteroid 
+import time
 
 def main():
-  print('Press space to start')
+    print('Press space to start')
+    
+    
+    
+    #Initialisation
+    # Création d'un astéroïde
+    asteroid = Asteroid(100, 100, 3)
+    # Au début de la boucle de jeu
+    start_time = time.time()
+    # Boucle de jeu
+    while True:
+        # Mise à jour du mouvement de l'astéroïde
+        
+        # À chaque mise à jour du jeu
+        current_time = time.time()
+        delta_time = current_time - start_time
+        start_time = current_time
+        
+        asteroid.update(delta_time)
 
-  pygame.init()
 
-  ecran = pygame.display.set_mode((640, 480))
+main()
